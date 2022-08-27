eed7bb1d53f40ce64c23b39c1ea83c8724146621
Gedeon Kakona <gedeonkakona@gmail.com>
2022-08-27 12:15:44 +0200

    cleaning code

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ import styles from './style';
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
+    <div className={`${styles.paddingX} ${styles.flexCenter} `}>
+      <div className={`${styles.boxWidth} `}>
         <Navbar />
       </div>
     </div>
